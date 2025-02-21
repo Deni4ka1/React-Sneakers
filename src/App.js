06bf5432ead02492d1c90b45bca5fd3537f98776
Deni4ka <atamaniukdenysv@gmail.com>
2025-02-21 21:41:22 +0200

    first react website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div className="wrapper">
+    <header>
+      <div className="headerLeft">
+        <img className="logo-img" src="/images/logo-img.png" />
+        <div className="hederInfo">
+          <h3 className="logo-name">React Sneakers</h3>
+          <p>Магазин найкращих кросівок</p>
+        </div>
+      </div>
+      <ul className="headerRight">
+        <li>
+          <img className="cart-img" src="/images/cart.svg" />
+          <span> 1205 грн.</span>
+        </li>
+        <li>
+          <img className="like-img" src="/images/like.svg" />
+          <span> Закладки</span>
+        </li>
+        <li>
+          <img className="user-img" src="/images/user.svg" />
+          <span> Профіль</span>
+        </li>
+      </ul>
+    </header>
+    <div className="contert">
+      <h1>Всі кросівки</h1>
     </div>
-  );
+  </div>
 }
 
 export default App;
